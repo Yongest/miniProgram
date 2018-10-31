46a06686771ac5dec5876c1cf3a38e7d1f0274c8
yongest <1046788379@qq.com>
2018-10-31 18:06:22 +0800

    项目结构搭建

---
+++ pages/home/home.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      '../imgs/2.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+})

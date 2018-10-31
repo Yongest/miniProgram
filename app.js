ec51fe596a0a489ae6c73274b07faf4d9a0f8671
yongest <1046788379@qq.com>
2018-10-31 23:38:45 +0800

    生命周期研究，http请求使用

--- app.js
+++ app.js
@@ -7,27 +7,49 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
+    console.log('我启动了')
+    // 获取登陆凭证
+    wx.login({
+      success(res){       //res.code 微信提供的临时凭证，发送code到后台获取openId,sessionKey,unionId
+        console.log(res)
+      }
+    }) 
+    // 获取用户信息 
+    wx.getUserInfo({   //需要通过另外的方式获取  https://developers.weixin.qq.com/community/develop/doc/0000a26e1aca6012e896a517556c01
+      success(res){
+        console.log(res)
+      }
+    })
+// 获取用户设置，授权信息等
+    wx.getSetting({
+      success(res){
+        console.log(res)
+      }
+    })
+
+    wx.getLocation({
+      success: function(res) {console.log(res)},
+    })
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+    console.log('我显示了')
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
   onHide: function () {
-    
+    console.log('我隐藏了')
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    
+    console.log('我eoor了')
   }
 })
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -11,55 +11,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    console.log('页面加载完毕')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    console.log('页面初次渲染完成')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+    console.log('页面显示')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+    console.log('页面隐藏')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+    console.log('页面卸载')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+    console.log('下拉动作')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+    console.log('到达底部')
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+    console.log('分享')
   }
 })
